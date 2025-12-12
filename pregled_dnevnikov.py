f08b4a767deaa699c79a342f7fb6df86957145fa
jernejp21 <jernej.pangerc@tutamail.com>
2025-12-12 16:15:08 +0100

    Dodal program za pregled dnevnikov.

---
+++ pregled_dnevnikov.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = np.genfromtxt('KROG2_1.CSV', delimiter=',', names=True, dtype=float)
+
+t_zel = data['T_zelena']
+t_tren = data['T_tren']
+ventil = data['ventil']
+cas = np.linspace(1, len(data['T_zelena']), len(data['T_zelena'])) 
+
+plt.figure()
+plt.plot(cas, t_tren, label="Trenutna temperatura")
+plt.plot(cas, t_zel, label="Želena temperatura", linestyle='--')
+plt.plot(cas, ventil, label="Položaj ventila", linestyle='dotted')
+plt.xlabel("Čas [sekunde]")
+plt.ylabel("Temperatura / položaj ventila")
+plt.title("Pregled dnevnika ogrevanja")
+plt.legend()
+plt.grid(True)
+plt.show()
