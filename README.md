152beb8aaeb0ce14c90e5d784e006b09883b6ef8
jernejp21 <jernej.pangerc@tutamail.com>
2026-01-08 13:23:59 +0100

    Popravil README.md

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# ogrevanje
-Krmilnik za ogrevanje hiše.
+# Krmilnik za centralno ogrevanje
+
+Krmilnik je narejen za preprosto krmiljenje, kjer je zalogovnik dovolj velik, da temperetaura ne pada kljub hitrim odpiranjem ventilov.
+
+Za boljšo regulacijo je potrebno popraviti krmilje. Problem je tudi zajem temperature, ki ni stabilen.
--- pregled_dnevnikov.py
+++ pregled_dnevnikov.py
@@ -3,10 +3,10 @@ import numpy as np
 
 data = np.genfromtxt('KROG2_1.CSV', delimiter=',', names=True, dtype=float)
 
-t_zel = data['T_zelena']
+t_zel = data['T_zeljena']
 t_tren = data['T_tren']
 ventil = data['ventil']
-cas = np.linspace(1, len(data['T_zelena']), len(data['T_zelena'])) 
+cas = np.linspace(1, len(data['T_zeljena']), len(data['T_zeljena'])) 
 
 plt.figure()
 plt.plot(cas, t_tren, label="Trenutna temperatura")
